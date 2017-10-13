0055832138e67b389c3c1466ec323dea51747b08
Vishal <vis8051@gmail.com>
2017-10-14 05:07:32 +1100

    Unit tests added

--- LeetCode-Solutions/LeetCode-Solutions/Backtracking.swift
+++ LeetCode-Solutions/LeetCode-Solutions/Backtracking.swift
@@ -104,6 +104,21 @@ func subString(_ s:String,_ start:Int,_ end:Int ) -> String {
     return s.substring(with: range)
 }
 
+
+/*
+Given a pattern and a string str, find if str follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty substring in str.
+
+Examples:
+pattern = "abab", str = "redblueredblue" should return true.
+pattern = "aaaa", str = "asdasdasdasd" should return true.
+pattern = "aabb", str = "xyzabcxzyabc" should return false.
+Notes:
+You may assume both pattern and str contains only lowercase letters.
+*/
+
+
 func word_break(_ s:String,_ start:Int ,_ str:inout [String] ) -> [String] {
     var res:[String] = []
     if ( start == s.count ) {
